035140f24026f11b253de1f26f77e1a7f8d280eb
PatTheEvil <125569368+PatTheEvil@users.noreply.github.com>
2023-12-27 18:53:05 +0100

    Update

--- BSCH2-Novotny/BSCH2-Novotny/obj/Debug/net6.0-windows/AddEditVojak.g.i.cs
+++ BSCH2-Novotny/BSCH2-Novotny/obj/Debug/net6.0-windows/AddEditVojak.g.i.cs
@@ -87,13 +87,13 @@ namespace BSCH2_Novotny {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BSCH2-Novotny;component/addeditvojak.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BSCH2-Novotny;V1.0.0.0;component/addeditvojak.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AddEditVojak.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -103,7 +103,7 @@ namespace BSCH2_Novotny {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
